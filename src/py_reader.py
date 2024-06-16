6fdfd7295bbabedf56cbc662f635c2004c45389b
Wesley <pl450b@outlook.com>
2024-06-16 16:30:50 -0400

    updates

--- src/py_reader.py
+++ src/py_reader.py
@@ -1,11 +1,14 @@
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+from collections import deque
 
 import time
 
 fifo_path = "/tmp/gesture_fifo"
-entry_list = []
+entry_list = ["", "", "", "", "", "", "", ""]
+
+line_map = [0, 10, 20, 30, 40, 50, 60]
 
 serial = i2c(port=1, address=0x3C)
 device = ssd1306(serial, rotate=0)
@@ -17,17 +20,21 @@ def read_fifo():
 
 def new_line(text, entries):
     entries.append(text)
-    recent_entries = entries[-4:]
-    for i in range(4):
-        current_line = recent_entries[i]
-        with canvas(device) as draw:
-            draw.text((1,64-i*10), current_line, fill="white")
+    recent_entries = entries[-7:]
+    
+    with canvas(device) as draw:
+        for i in range(7):
+            draw.text((0, line_map[i]), recent_entries[6-i], fill="white")
     time.sleep(1)
 
 if __name__ == "__main__":
     new_line("test 1", entry_list)
-    new_line("Test_2", entry_list)
-    new_line("testtttt 3", entry_list)
-    new_line("test  test  4", entry_list);
+    new_line("test 2", entry_list)
+    new_line("test 3", entry_list)
+    new_line("test 4", entry_list)
+    new_line("test 5", entry_list)
+    new_line("test 555555", entry_list)
+    new_line("test 7", entry_list)
+    new_line("test 8", entry_list)
 
-          
+    time.sleep(10)
