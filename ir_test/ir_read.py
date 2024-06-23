83597699d1266762de33f934c3d0d1d28899985c
Wesley Flynn <pl450b@pl450b-main.flynn.net>
2024-06-22 20:29:26 -0400

    test read bool

--- ir_test/ir_read.py
+++ ir_test/ir_read.py
@@ -1,7 +1,13 @@
+import time
+
 fifo_path = "/tmp/gesture_fifo"
 
 while True:
     with open(fifo_path, 'r') as file:
         ir_data = file.read().strip()
-    print(ir_data)
+        if(ir_data):
+            print("Data coming in!")
+        else:
+            print("No data :(")
+    time.sleep(1)
 
