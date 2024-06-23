4a3b589650b2b7a1d4cf1e1b711d52be94998645
Wesley Flynn <pl450b@pl450b-main.flynn.net>
2024-06-22 21:23:36 -0400

    first try at thread syncing

--- src/main.py
+++ src/main.py
@@ -1,13 +1,32 @@
 from .wrist_screen import WristScreen
+
 import time
+import threading
+
+fifo_path = "/tmp/gesture_fifo"
+
+def read_ir(path, lock, screen):
+    while True:
+        file = open(path, 'r')
+        lock.acquire()
+        ir_data = file.read().strip()
+        if(ir_data == "999"):
+            lock.release()
+        screen.new_line("SYSTEM", ir_data)
+
+screen_lock = threading.Lock()
 
 if __name__ == "__main__":
-    screen1 = WristScreen()
-    count = 0
+    sys_screen = WristScreen()
+    test_screen = WristScreen()
+
+    thread1 = threading.Thread(target=read_ir, args=(fifo_path, screen_lock, sys_screen))
 
     while True:
-        screen1.new_line("TESTER", f"test {count}")
-        count += 1
-        time.sleep(0.2)
+        screen_lock.acquire()
+        test_screen.new_line("TEST", "test from wes")
+        screen_lock.release()
+        time.sleep(3)
+
 
 
